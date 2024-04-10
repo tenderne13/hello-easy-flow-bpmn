ae881983b3fdb2d25059bd650c2b0c869ccaee5c
lixp <lixp7@xiaopeng.com>
2024-04-10 18:08:44 +0800

    init

---
+++ src/main/java/com/xiaopeng/workflow/HelloEasyFlowBpmnApplication.java
@@ -0,0 +1,13 @@
+package com.xiaopeng.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEasyFlowBpmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEasyFlowBpmnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaopeng/workflow/HelloEasyFlowBpmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaopeng.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEasyFlowBpmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
