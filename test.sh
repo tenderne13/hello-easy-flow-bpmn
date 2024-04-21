9ba278d4becea727c82880bb616babd8de550ca3
lixp <lixp7@xiaopeng.com>
2024-04-21 11:21:15 +0800

    test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home
 ## mvn clean test -D test=com.xiaopeng.workflow.HelloEasyFlowBpmnApplicationTests#testConvertXPComp -e
-mvn clean test -D test=com.xiaopeng.workflow.HelloEasyFlowBpmnApplicationTests#testConditionXPComp -e
+mvn clean test -D test=com.xiaopeng.workflow.HelloEasyFlowBpmnApplicationTests
